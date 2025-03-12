ce585c58ed3bc63a071ba29c231d62768ac9174f
ali <alitoson@gmail.com>
2025-03-12 14:59:36 +0200

    Create function of add note cubit

--- lib/Cubits/add_notes_cubit/add_notes_cubit.dart
+++ lib/Cubits/add_notes_cubit/add_notes_cubit.dart
@@ -1,12 +1,23 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/Cubits/add_notes_cubit/add_notes_states.dart';
 import 'package:notes_app/Models/notes_model.dart';
 
 class NotesCubit extends Cubit<NotesStates> {
   NotesCubit() : super(initialStates());
 
-  void addNotesModel({required NotesModel notesmodel})
-  {
-
+  void addNotesMethod({required NotesModel notesmodel}) async {
+    emit(NotesLoadingStates());
+    try {
+      var hiveBox = Hive.box<NotesModel>('notes_box');
+      await hiveBox.add(notesmodel);
+      emit(NotesSuccessStates());
+    } catch (e) {
+      emit(
+        NotesfailedStates(
+          messageError: e.toString(),
+        ),
+      );
+    }
   }
 }
--- lib/Views/home_view.dart
+++ lib/Views/home_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/Cubits/add_notes_cubit/add_notes_cubit.dart';
 import 'package:notes_app/Widgets/Custom_model_buttom_sheet.dart';
 import 'package:notes_app/Widgets/custom_notes_body.dart';
 
@@ -8,19 +10,20 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomNotesBody(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-            backgroundColor: Color(0xff323232),
-            context: context,
-            builder: (context) {
-              return CustomModelButtomSheet();
-            },
-          );
-        },
-        child: Icon(Icons.add),
-      ),
+        body: CustomNotesBody(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+              backgroundColor: Color(0xff323232),
+              context: context,
+              builder: (context) {
+                return CustomModelButtomSheet();
+              },
+            );
+          },
+          child: Icon(Icons.add),
+        ),
+      
     );
   }
 }
